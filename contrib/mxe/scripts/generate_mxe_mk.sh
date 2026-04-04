45c999f0ac178c493cb39cd45ba706421c99ffe8
Harry Kunz <harry.kunz@ymail.com>
2026-04-04 23:35:48 +0800

    use lowercase for MXE package name

--- contrib/mxe/scripts/generate_mxe_mk.sh
+++ contrib/mxe/scripts/generate_mxe_mk.sh
@@ -106,6 +106,7 @@ decho "Additional MXE args: $MXE_ARGS"
 if [[ "$MXE_ARGS" == "default" || -z "$MXE_ARGS" ]]; then
     PACKAGE_NAME_MXE=$PACKAGE_NAME
     PACKAGE_NAME_MXE="$(echo "$PACKAGE_NAME_MXE" | sed -E 's/(\.mk|\.tar\.gz|\.tgz|\.zip|\.tar\.bz2|\.tar\.xz)$//')"
+    PACKAGE_NAME_MXE=$(echo "$PACKAGE_NAME_MXE" | tr '[:upper:]' '[:lower:]')
     iecho "No package name provided; default generation with name $(bold_bright_green "$PACKAGE_NAME_MXE")"
 else
     PACKAGE_NAME_MXE="${MXE_ARGS%%,*}"  # everything before first comma
